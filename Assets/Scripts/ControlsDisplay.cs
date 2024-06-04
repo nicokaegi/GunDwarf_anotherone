ae250d3132ca720a9e52a97e45bb46b6677545cf
Mathias Øgaard <mapiza00@gmail.com>
2024-06-04 19:06:11 +0200

    Added option to display controls in PauseMenu, and changed the aesthetics of the menu

---
+++ Assets/Scripts/ControlsDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class ControlsDisplay : MonoBehaviour
+{
+    public GameObject controlsDisplayUI;
+
+    // Start is called before the first frame update
+    void start()
+	{
+		//TODO
+	}
+
+    // Update is called once per frame
+    void update()
+	{
+		//TODO
+	}
+
+	public void showUI()
+	{
+		controlsDisplayUI.SetActive(true);
+	}
+
+	public void hideUI()
+	{
+		controlsDisplayUI.SetActive(false);
+	}
+}
